709cd17a53e26f4d3bae9a83588efb73b86cf06a
Andrew C <capatina.andrew@gmail.com>
2020-04-23 01:20:48 -0700

    Added initial parsing ability.

--- qor_parser.py
+++ qor_parser.py
@@ -6,6 +6,28 @@
         list with values to be loaded into an excel spreadsheet. 
 
 """
+
+def get_timing_paths(qor_report):
+    """
+        Function to parse and get all timing paths in the QOR report.
+
+        input: qor_report - text file containing contents 
+                            to search for. 
+
+        output: timing_paths - timing paths of the current design.
+    """
+    timing_paths = []
+    for line in qor_report:
+        rtn = line.find('Timing Path Group')
+        if rtn != -1:
+            line = line.strip('Timing Path Group')
+            line = line.strip("'")
+            line = line.strip()
+            line = line[:-1]
+            timing_paths.append(line)
+
+    return timing_paths
+
 def read_file(file_path):
     """
         Function to read file and return the whole file.
@@ -14,7 +36,7 @@ def read_file(file_path):
         output: qor_report - file contents returned.
     """
     with open(file_path) as fp:
-        qor_report = f.read()
+        qor_report = fp.readlines()
 
     return qor_report
 
@@ -22,9 +44,11 @@ def read_file(file_path):
 def main():
     print("Starting QOR parser.")
 
-    file_path = ""
+    file_path = "ORCA_TOP.cts2.qor.rpt"
     qor_report = read_file(file_path)
-    print(qor_report)
+    timing_paths = get_timing_paths(qor_report)
+    print(timing_paths)
+
 
 
 if __name__ == "__main__":
