4338872b566d4d5eb2bf02428b58a4ebe7c13da2
Andrew C <capatina.andrew@gmail.com>
2020-04-25 14:38:16 -0700

    Updating repository with code to parse both clock_qor and qor

--- qor_parser.py
+++ qor_parser.py
@@ -1,27 +1,31 @@
 """
     Andrew Capatina
-
     Description:
         This script parses a QOR report and creates a comma seperated 
         list with values to be loaded into an excel spreadsheet. 
-
 """
 
 import glob
 import csv
 
 
+# Update the below global variables if qor.rpt or clock_qor.rpt
+# file reporting format changes. 
+
 hold_violation_str = "Worst Hold Violation:"
 total_hold_violation_str = 'Total Hold Violation:'
 total_neg_slack_str = 'Total Negative Slack:'
 wns_str = 'Critical Path Slack:'
 
+# Labels used for columns of clock_qor report.
+column_labels_clock_qor = ['Clock/Skew Group', 'Attrs', 'Sinks', 'Levels', 'Clock Repeater Count',
+                            'Clock Repeater Area', 'Clock Stdcell Area', 'Max Latency',
+                            'Global Skew', 'Trans DRC Count', 'Cap DRC Count']
 
 def get_hold_times(qor_report):
     """
         Function to get worst hold time violations
         for each clock group.
-
         input: qor_report - text file containing contents 
                             to search for. (list)
         output: hold_times - list of times taken from report.
@@ -40,10 +44,8 @@ def get_hold_times(qor_report):
 def get_timing_paths(qor_report):
     """
         Function to parse and get all timing paths in the QOR report.
-
         input: qor_report - text file containing contents 
                             to search for. (list) 
-
         output: timing_paths - list of timing paths of the current design.
     """
     timing_paths = []
@@ -62,7 +64,6 @@ def get_timing_paths(qor_report):
 def get_tns(qor_report):
     """
         Function to get all total negative slack times in QOR.
-
         input: qor_report - list containing lines of file.
         output: tns_times - list containing times from file.
     """
@@ -80,7 +81,6 @@ def get_tns(qor_report):
 def get_wns(qor_report):
     """
         Function to get all worst negative slack times from QOR.
-
         input: qor_report - list containing lines of file.
         output: wns_times - list of slack times.
     """
@@ -98,7 +98,6 @@ def get_wns(qor_report):
 def get_worst_hold_violation(qor_report):
     """
         Function to get worst hold violation time from qor.
-
         input: qor_report - List containing lines of file.
         output: worst_hold_vio - Worst hold violation list. 
     """
@@ -112,10 +111,34 @@ def get_worst_hold_violation(qor_report):
     return worst_hold_vio
 
 
+def parse_clock_qor(qor_report):
+    """
+        Function to parse clock_qor.
+        input: qor_report: list containing report. 
+    """
+    clocks = []
+    for line in qor_report:
+        rtn = line.find('CLK')
+        rtn_2 = line.find('Summary Table for')
+        if rtn_2 != -1:
+            line = line.strip()
+            line = line.strip("=")
+            clocks.append(line)
+        if rtn != -1:
+            clocks.append(line)
+        
+    clock_qor = []
+    for clock in clocks:
+        clock = clock.split()
+        if(len(clock) > 3): # This is done to filter out unneeded elements of list.
+            clock_qor.append(clock)
+    
+    return clock_qor
+
+
 def read_file(file_path):
     """
         Function to read file and return the whole file.
-
         input: file_path - file path to qor files.
         output: qor_report - file contents returned.
     """
@@ -125,20 +148,21 @@ def read_file(file_path):
     return qor_report
 
 
-def write_to_csv(print_row):
+def write_to_csv(file_path, reports):
     """
         Function to write the argument in a CSV file.
-
         input: print_row: list of lists containing timing and header information.
     """
-    with open('report_qor_parsed.csv', 'w') as csvfile:
+    with open(file_path + '_parsed.csv', 'w') as csvfile:
         qor_writer = csv.writer(csvfile)
-        for row in print_row:
-            qor_writer.writerow(row)
+        for report in reports:
+            for row in report:
+                qor_writer.writerow(row)
 
 
 def main():
-    for file_path in glob.glob("*qor*.rpt"):
+    for file_path in glob.glob("*.qor.rpt"):
+
         print("Parsing file " + file_path)
         qor_report = read_file(file_path)
         timing_paths = get_timing_paths(qor_report)
@@ -152,10 +176,18 @@ def main():
         tns_times.insert(0, total_neg_slack_str)
         worst_hold_vio.insert(0, hold_violation_str)
         hold_times.insert(0, total_hold_violation_str)
-        print_row = [timing_paths, wns_times, tns_times, 
+        report_qor = [timing_paths, wns_times, tns_times, 
                      worst_hold_vio, hold_times]
         
-        write_to_csv(print_row)
+    for file_path in glob.glob("*clock_qor.rpt"):
+        print("Parsing file " + file_path)
+        qor_report = []
+        qor_report = read_file(file_path)
+        clock_qor = parse_clock_qor(qor_report)
+        clock_qor.insert(1, column_labels_clock_qor)
+
+    print_row = [report_qor,clock_qor]
+    write_to_csv(file_path, print_row)
 
 
 if __name__ == "__main__":
